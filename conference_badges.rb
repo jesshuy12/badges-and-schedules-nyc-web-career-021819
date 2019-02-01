6249dd0017104818893e909bd1c7703a0036ccbc
Jessy Hu <jesshuy12@gmail.com>
2019-02-01 20:06:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -15,7 +15,7 @@ end
 
 def assign_rooms(name)
   name.each.with_index do |name, index|
-    "Hello, #{name}! You'll be assigned to room #{room}!"
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
   end
 end
   
